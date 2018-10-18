54fbb7d9d220c1d0d5917439d0a1390427ce07b1
Dacrol <jonas.ahrens@gmail.com>
2018-10-18 12:31:22 +0200

    Change / to .

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('/'))
+app.use(express.static('.'))
 
 app.listen(process.env.PORT || 3200, () => {
   console.log(`Listening on port ${process.env.PORT || 3200}`)
